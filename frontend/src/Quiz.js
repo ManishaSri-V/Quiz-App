0c8298a4850eda16614374cb2e1264df01206e1d
vishwajithBarad1 <vishwajithbarad@gmail.com>
2024-08-18 14:19:05 +0530

    fixed the scoreCard not showing the score.

--- frontend/src/Quiz.js
+++ frontend/src/Quiz.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const Quiz = () => {
   const [quizzes, setQuizzes] = useState([]);
-  const [answers, setAnswers] = useState([]);
+  const [answers, setAnswers] = useState({});
   const [score, setScore] = useState(null);
 
   const fetchQuestions = async () => {
@@ -15,24 +15,40 @@ const Quiz = () => {
       alert("some error occured while fetching questions");
     }
   };
-  console.log("These are my Questions", quizzes);
 
   const handleAnswerChange = (index, optionIndex) => {
-    const newAnswers = [...answers];
+    const newAnswers = {...answers};
     newAnswers[index] = optionIndex;
     setAnswers(newAnswers);
   };
 
-  const handleSubmit = async () => {
-    const response = await axios.post("http://localhost:5050/api/quiz/submit", {
-      answers,
-    });
+  // const handleSubmit = async () => {
+  //   const response = await axios.post("http://localhost:5050/api/quiz/submit", {
+  //     answers,
+  //   });
+  //   setScore(response.data);
+  // };
 
-    setScore(response.data);
-  };
-
-  console.log("Error occured submitting answers");
-
+  const handleSubmit = () =>{
+    const allAnswersStrngs = quizzes.map((question)=>{return question.correctOption});
+    const allAnswers = allAnswersStrngs.map((answer)=>{return [ answer.split(' ')[1]?answer.split(' ')[1]:"A)", answer.split(' ')[0]];});
+    const allCorreectChoise = []
+    for(let array in allAnswers){
+      if(allAnswers[array][0].includes(")") && "ABCD".includes(allAnswers[array][0][0])){
+        allCorreectChoise.push(allAnswers[array][0][0]);
+      }
+      if(allAnswers[array][1].includes(")") && "ABCD".includes(allAnswers[array][1][0])){
+        allCorreectChoise.push(allAnswers[array][1][0]);
+      }
+    }
+    let total = 0
+    for(let i=0;i<allCorreectChoise.length;i++){
+      if(allCorreectChoise[i] == "ABCD"[answers[i]]){
+        total++;
+      }
+    }
+    setScore({score:total,total:allCorreectChoise.length});
+  }
   return (
     <div>
       <h1>Quiz</h1>
